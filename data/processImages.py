af4d8c6a599891451e13fc7c0683817f5a82d538
Magicpig55 <kelott5566@gmail.com>
2020-11-12 15:51:25 -0700

    Resize cropped images to 256x256, move some images from cracked to intact

--- data/processImages.py
+++ data/processImages.py
@@ -3,41 +3,43 @@ import sys, math, os
 from pathlib import Path
 
 
+
 def process_image(imurl, output):
     im = Image.open(imurl).convert("RGB")
     p = Path(imurl)
     width, height = im.size
+    resolution = 256
     if height > width:
         print("Portrait Image")
-        if width > 1440:
-            ratio = 1440 / width
+        if width > resolution:
+            ratio = resolution / width
             newHeight = math.floor(height * ratio)
-            im = im.resize((1440, newHeight))
+            im = im.resize((resolution, newHeight))
             height = newHeight
-            width = 1440
+            width = resolution
             print("Resized to " + str(height) + "," + str(width))
-        im1 = im.crop((0, 0, width, 1440))
-        im2 = im.crop((0, height - 1440, width, height))
+        im1 = im.crop((0, 0, width, resolution))
+        im2 = im.crop((0, height - resolution, width, height))
         im1.save(output + p.stem + "_1.jpg")
         im2.save(output + p.stem + "_2.jpg")
 
     elif height < width:
         print("Landscape Image")
-        if height > 1440:
-            ratio = 1440 / height
+        if height > resolution:
+            ratio = resolution / height
             newWidth = math.floor(width * ratio)
-            im = im.resize((newWidth, 1440))
+            im = im.resize((newWidth, resolution))
             width = newWidth
-            height = 1440
+            height = resolution
             print("Resized to " + str(height) + "," + str(width))
-        im1 = im.crop((0, 0, 1440, height))
-        im2 = im.crop((width - 1440, 0, width, height))
+        im1 = im.crop((0, 0, resolution, height))
+        im2 = im.crop((width - resolution, 0, width, height))
         im1.save(output + p.stem + "_1.jpg")
         im2.save(output + p.stem + "_2.jpg")
     else:
         print("Square Image")
-        if height != 1440:
-            im = im.resize((1440, 1440))
+        if height != resolution:
+            im = im.resize((resolution, resolution))
         im.save(output + p.stem + ".jpg")
 
 
